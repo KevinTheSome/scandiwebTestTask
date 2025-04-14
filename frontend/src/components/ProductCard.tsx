df32d7575f3932f775e9d6d4fc75bc9f895ba9fc
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2025-04-14 19:32:22 +0300

    Ah good enought ;)

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ProductType } from "../types/ProductType";
 function productCard({ product }: { product: ProductType }) {
   return (
-    <div className="bg-white w-1/3 h-1/3">
+    <div className="bg-white w-full h-full grid hover:shadow-lg hover:bg-gray-300">
       <img src="{}" alt="{}" />
       <h3 className="font-extralight p-4 text-md">{product.name}</h3>
       <p className="text-sm">{product.prices}</p>
